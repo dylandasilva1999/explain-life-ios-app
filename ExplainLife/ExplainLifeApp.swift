355ae42cc00a5b22d7e589893f996712fbac7c00
Dylan da Silva <dylandasilva.b@gmail.com>
2021-09-28 16:21:30 +0200

    Add Firebase To Project (Working)

--- ExplainLife/ExplainLifeApp.swift
+++ ExplainLife/ExplainLifeApp.swift
@@ -6,12 +6,31 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ExplainLifeApp: App {
+    
+    //Create an instance of AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+//App Delegate Class
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        //Print Firebase loading verification
+        print("Firebase loading...")
+        
+        //Initialise Firebase
+        FirebaseApp.configure()
+        
+        //Return true
+        return true
     }
 }
