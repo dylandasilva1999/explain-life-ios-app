2a34003cfccc964e776c043c7469acb42525a455
Dylan da Silva <dylandasilva.b@gmail.com>
2021-09-28 11:30:23 +0200

    Initial Commit

---
+++ ExplainLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExplainLife
+//
+//  Created by Dylan da Silva on 2021/09/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExplainLife/ExplainLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplainLifeApp.swift
+//  ExplainLife
+//
+//  Created by Dylan da Silva on 2021/09/28.
+//
+
+import SwiftUI
+
+@main
+struct ExplainLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
