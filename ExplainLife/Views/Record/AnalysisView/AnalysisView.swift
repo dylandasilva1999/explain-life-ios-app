cb3c3437685d33bdb9ddd1e3beedf9cb70b4135f
Dylan da Silva <dylandasilva.b@gmail.com>
2021-10-12 21:47:20 +0200

    Getting IBM ToneAnalyzer Response
    -Speech-to-text is analysed and getting a response in the console.

--- ExplainLife/Views/Record/AnalysisView/AnalysisView.swift
+++ ExplainLife/Views/Record/AnalysisView/AnalysisView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import ToneAnalyzer
 
 struct AnalysisView: View {
     
@@ -26,9 +27,41 @@ struct AnalysisView: View {
             }, label: {
                 Text("Dismiss Modal")
             })
+            
+            Button(action: {
+                self.configureToneAnalyzer()
+            }, label: {
+                Text("Analyse")
+            })
+                .padding(.top, 200)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color("Pastel Purple"))
         .edgesIgnoringSafeArea(.all)
+    }
+    
+    // Method to configure the Tone Analyzer SDK
+    func configureToneAnalyzer() {
+        let authenticator = WatsonIAMAuthenticator(apiKey: "Hg2LSkfvwpVS3EfelDBMRUwXaLBQ_oYL-LDJem8hDD2P")
+        let toneAnalyzer = ToneAnalyzer(version: "2017-09-21", authenticator: authenticator)
+        toneAnalyzer.serviceURL = "https://api.au-syd.tone-analyzer.watson.cloud.ibm.com/instances/1956e6e2-d646-40cf-8bb5-33ad0ac7f322"
+
+        let utterances = [
+            Utterance(text: swiftUISpeech.analysisText),
+        ]
+
+        toneAnalyzer.toneChat(
+          utterances: utterances,
+          acceptLanguage: "en")
+        {
+          response, error in
+
+          guard let analyses = response?.result else {
+            print(error as Any)
+            return
+          }
+          
+          print(analyses)
+        }
     }
 }
--- Podfile
+++ Podfile
@@ -19,3 +19,4 @@ target 'ExplainLife' do
   # add pods for any other desired Firebase products
   # https://firebase.google.com/docs/ios/setup#available-pods
 end
+
