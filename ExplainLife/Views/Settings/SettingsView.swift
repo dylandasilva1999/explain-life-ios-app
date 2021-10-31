9190d6c8c88162e4fc66540812aeba00693f55d4
Dylan da Silva <dylandasilva.b@gmail.com>
2021-10-31 13:37:09 +0200

    Add Donate & Contact Support Cards
    - Added URL links to Donate to Autism South Africa, and a link to Contact Support at Autism South Africa.

--- ExplainLife/Views/Settings/SettingsView.swift
+++ ExplainLife/Views/Settings/SettingsView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @Environment(\.openURL) var openURL
+    
     var body: some View {
         VStack(spacing: 40) {
             VStack(spacing: 20) {
@@ -31,17 +34,97 @@ struct SettingsView: View {
                     content: { Text("edit and personalise the app to your needs by clicking on the appropriate item below.")
                             .font(Font.custom("Aeonik-Regular", size: 18))
                             .foregroundColor(Color("Navy Blue"))
-                })
-                .cornerRadius(20)
-                .groupBoxStyle(TransparentGroupBoxSettings())
+                    })
+                    .cornerRadius(20)
+                    .groupBoxStyle(TransparentGroupBoxSettings())
             }
             .frame(width: UIScreen.main.bounds.width - 80)
+            
+            HStack {
+                Button(action: {
+                    openURL(URL(string: "https://aut2know.co.za/how-to-donate/")!)
+                }) {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName: "globe.asia.australia.fill")
+                                .renderingMode(.template)
+                                .font(.title)
+                                .foregroundColor(Color("Navy Blue"))
+                            
+                            Spacer()
+                        }
+                        .padding(.leading, 22)
+                        .padding(.trailing, 22)
+                        .padding(.top, 22)
+                        .padding(.bottom, 2)
+                        
+                        HStack {
+                            Text("Donate")
+                                .font(Font.custom("Aeonik-Bold", size: 20))
+                                .foregroundColor(Color("Navy Blue"))
+                            
+                            Spacer()
+                        }
+                        .padding(.leading, 22)
+                        .padding(.trailing, 22)
+                        
+                        Text("Autism SA")
+                            .font(Font.custom("Aeonik-Regular", size: 16))
+                            .foregroundColor(Color("Navy Blue"))
+                            .padding(.leading, 22)
+                            .padding(.trailing, 22)
+                            .padding(.bottom, 22)
+                    }
+                }
+                .background(Color("Muted Pastel Pink"))
+                .cornerRadius(20)
+                
+                Button(action: {
+                    openURL(URL(string: "https://aut2know.co.za/contact-us-2/")!)
+                }) {
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Image(systemName: "phone.fill")
+                                .renderingMode(.template)
+                                .font(.title)
+                                .foregroundColor(Color("Navy Blue"))
+                            
+                            Spacer()
+                        }
+                        .padding(.leading, 22)
+                        .padding(.trailing, 22)
+                        .padding(.top, 22)
+                        .padding(.bottom, 2)
+                        
+                        HStack {
+                            Text("Need help?")
+                                .font(Font.custom("Aeonik-Bold", size: 20))
+                                .foregroundColor(Color("Navy Blue"))
+                            
+                            Spacer()
+                        }
+                        .padding(.leading, 22)
+                        .padding(.trailing, 22)
+                        
+                        Text("Contact Support")
+                            .font(Font.custom("Aeonik-Regular", size: 16))
+                            .foregroundColor(Color("Navy Blue"))
+                            .padding(.leading, 22)
+                            .padding(.trailing, 22)
+                            .padding(.bottom, 22)
+                    }
+                }
+                .background(Color("Muted Pastel Pink"))
+                .cornerRadius(20)
+            }
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(maxWidth: UIScreen.main.bounds.width - 80)
             
             Spacer()
             
             //Say out loud button
             Button(action: {
-
+                
             }) {
                 Text("sign out of account")
                     .font(Font.custom("Aeonik-Regular", size: 25))
